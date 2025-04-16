74656162392572cf4506b0a55374b4f50d1d5109
snarolia <siddharthnarolia@gmail.com>
2025-04-16 10:19:50 +0530

    cycle operations completed

--- CycleDetection.py
+++ CycleDetection.py
@@ -1,5 +1,7 @@
 from LinkedList import Node, LinkedList
 
+
+## The functions returns boolean for cycle exists, length of cycle and meeting point of fast and slow pointers
 def hasCycle(list:LinkedList):
     slow=list.head
     fast=list.head
@@ -9,8 +11,32 @@ def hasCycle(list:LinkedList):
         slow=slow.next
 
         if (slow==fast):
-            return True
-    return False
+            cycle_length=calculateCycleLength(slow)
+            start_of_cycle=startOfCycle(list.head, cycle_length)
+            return True, cycle_length, slow, start_of_cycle
+    return False, None, None, None
+
+def calculateCycleLength(slow:Node):
+    current=slow
+    cycle_length=0
+    while True:
+        current=current.next
+        cycle_length+=1
+        if current==slow:
+            break
+    return cycle_length
+
+def startOfCycle(head:Node,cycle_length:int):
+    start=head
+    mover=head
+
+    for _ in range(cycle_length):
+        mover=mover.next
+    while start!=mover:
+        start=start.next
+        mover=mover.next
+    return mover
+
 
 
 def main():
@@ -25,7 +51,16 @@ def main():
     ## Commenting out list printing because it'll run infinetely
     #list.printList()
 
-    cycle = hasCycle(list)
-    print(cycle)
+    cycle, length, meet_node, cycle_start_node = hasCycle(list)
+
+    if cycle:
+        print(f"Cycle exists in LinkedList with length: ", end="-->")
+        print(length)
+        print("meeting node is : "+str(meet_node.data))
+        print("Cycle start at node: "+str(cycle_start_node.data))
+    else:
+        print("No cycle in LinkedList")
+
+
 if __name__== "__main__":
     main()
